d7d25d758163efce5022c53a465f15533c4a947e
Nitin Kant <kantnitin7@gmail.com>
2024-05-29 00:17:46 +0530

    initial commit

---
+++ src/app/products/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+
+export default function Products() {
+    
+  return (
+   <div>
+    <h1>Product list</h1>
+    {/* <button onClick={productPage}>products</button> */}
+   </div>
+  );
+}
