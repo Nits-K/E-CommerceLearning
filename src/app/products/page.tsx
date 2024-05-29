ca328b09bec3a2d81b68825ff0f926af277e69eb
Nitin Kant <kantnitin7@gmail.com>
2024-05-30 01:10:58 +0530

    products list added

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,12 +1,26 @@
-"use client"
+import Header from "@/app/components/Header";
+import Card from "@/app/components/card";
+import productlist from "@/app/assets/data/productlist.json";
 
-
-export default function Products() {
-    
+const products = () => {
   return (
-   <div>
-    <h1>Product list</h1>
-    {/* <button onClick={productPage}>products</button> */}
-   </div>
+    <div>
+      <Header />
+      {/* <Card/> */}
+      
+        <div className="card-list">
+        {productlist.map((product)=> (
+          <Card
+            id={product.id}
+            brand={product.brand}
+            size={product.size}
+            price={product.price}
+            image={product.image}
+          />
+        ))}
+      </div>
+      
+    </div>
   );
-}
+};
+export default products;
