cfab806c06a72d8a880080b565e7966a45948fcb
b4n1y4 <riteshgupta7645@gmail.com>
2024-12-08 12:31:18 +0530

    some changes

--- src/app/components/viewer.jsx
+++ src/app/components/viewer.jsx
@@ -1,21 +1,29 @@
-import {useState,Suspense} from 'react'
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls, useAspect } from '@react-three/drei';
+import { useState, Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, useAspect } from "@react-three/drei";
 
-import Tshirt from './Tshirt';
+import Denim from "./Denim";
+import SkirtT from "./Skirt_Tshirt";
 
-
-function Viewer({viewId}) {
+function Viewer({ viewId }) {
   const [count, setCount] = useState(0);
   return (
     <>
-    <Canvas>
-      <ambientLight />
-      <OrbitControls  />
-      <Suspense fallback={null}> 
-        <Tshirt/>
-       </Suspense> 
-    </Canvas>
+      <Canvas
+        camera={{
+          position: [800, 100, 100], // Adjust position [x, y, z] to get the desired view
+        }}
+      >
+        <ambientLight intensity={0.5} />
+        <OrbitControls
+          target={[1, 1, 1]} // Set the origin of the orbit controls
+          // enableZoom={false}
+        />
+        <Suspense fallback={null}>
+          <Denim />
+          {/* <SkirtT /> */}
+        </Suspense>
+      </Canvas>
     </>
   );
 }
